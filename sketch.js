0150b0665f91309869286e544cc257b333fde6a7
superyousuf <76240444+superyousuf@users.noreply.github.com>
2021-03-08 13:11:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,100 @@
+var cat;
+var jerry; 
+
+
+function preload() {
+    //load the images here
+
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+createCanvas=(800,400);
+cat=createSprite=(100,250,10,50);
+mouse=createSprite=(300,250,20,60)
+
+
+
+
+}
+
+function draw() {
+background=("garden.png");
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+if(cat.isTouching (mouse) ){
+    addAnimation(jerryImg2);
+}
+    drawSprites();
+}
+
+
+function keyPressed(){
+if(keyCode===left_Arrow){
+    cat .velocityX=-5;
+    cat.addAnimation("catRunning",catImg2);
+    cat.changeAnimation("catRunning");
++
++
+}
+
+  //For moving and changing animation write code here
+text(mouseX +','+ mouseY,10,45);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
